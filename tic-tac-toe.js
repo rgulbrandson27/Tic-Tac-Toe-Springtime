147233403c957e9958bb11534ab5af85d20d2078
rgulbrandson27 <105253984+rgulbrandson27@users.noreply.github.com>
2023-06-25 20:24:59 -0500

    made some good progress

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,70 +1,72 @@
 let gameActive = true;
 let currentPlayer = "Player X"
 
-let PlayerXArray = [];
-let PlayerOArray = [];
-
-function playGame() {
-   updateCellStatus();
-   checkForWinner();
-   declarWinner();
-   resetGame();
-
-
-}
+let playerXArray = [];
+let playerOArray = [];
 
 function updateCellStatus(e) {
 
    const x = document.createElement("img");
    x.setAttribute("src", "images/ButterflyX.png");
+   x.setAttribute("id", "butterfly");
 
    const o = document.createElement('img');
-   o.setAttribute("src", "images/FlowerO.png");
+   o.setAttribute("src", "images/FlowerOPastel.png");
+   o.setAttribute("id", "flower");
 
    if (currentPlayer === "Player X") {
-      $(e).append(x);
-      PlayerXArray.push((e.id.charAt(5)));
-      }  else { 
-      $(e).append(o);
-      PlayerOArray.push((e.id.charAt(5)));
-   }
+      e.append(x);
+      playerXArray.push((e.id.charAt(5)));
+      }  else {
+      e.append(o);
+      playerOArray.push((e.id.charAt(5)))
+      }
 
    e.removeAttribute("onclick");
-   console.log("Ive been clicked by " + currentPlayer);
-   console.log('Player X: [' + PlayerXArray +  '] & Player O: [ ' + PlayerOArray + ']');
+   
+   console.log(`Player X: [${playerXArray}] Player O: [${playerOArray}]`);
+   
+   currentPlayer = currentPlayer === "Player X" ? "Player O" : "Player X";
+
+   let winCombination = (
+      ('4', '5', '6'))
+      //('1', '2', '3'))
+   ;
+   
+      // (1,2,3) || 
+// (4 && 5 && 6) || 
+// (7,8,9) || 
+// (1,4,7) || 
+// (2,5,8) || 
+// (3,6,9) || 
+// (1,5,9) || 
+// (3,5,7)
+;
+   console.log(playerOArray.includes(winCombination));
+   //checkForWinner();
+ 
+}
+function resetGame(e) {
+   let allCells = $('.cell');
+   allCells.empty(e);
+   playerXArray = [];
+   playerOArray = [];  
+   console.log(`Player X: [${playerXArray}] Player O: [${playerOArray}]`);
+   console.log("Start New Game");
+   location.reload();
 }
 
-// function checkForWinner() 
-//    if (currentPlayerArray.includes(1)) {
-//        return true;{
-//    // console.log({checkForWinner});
-  
-//
- // currentPlayer = currentPlayer === "x" ? "x" : "o";
 
-
-// function changePlayer () {
-//    xPlaysNext = !xPlaysNext;
-
-
-
-
-
-
-   
-
-      // PlayerXArray.push((e.id.charAt(5)));        //
-      // console.log("I've been clicked by Player X");
-
-      // PlayerOArray.push((e.id.charAt(5)));        //
-      // console.log("I've been clicked by Player O");
-
-      // console.log('Player X: [' + PlayerXArray +  '] & Player O: [ ' + PlayerOArray + ']');
-     // checkForWinner();
- 
-
-
-
+//function checkForWinner() {
+   // if (playerXArray.includes('1') === true) {
+   //    console.log("X Wins") 
+   // }  else if 
+      //console.log(playerOArray.includes('3'))
+   //     {
+   //    console.log("O Wins")
+   // }  else {
+   //    console.log("No winner yet")
+   // }
 
 
 
@@ -80,8 +82,6 @@ function updateCellStatus(e) {
 //       || PlayerOArray).includes(winningCombination)
 
 
-
-
  //if (playerXArray.includes(('5')) && (playerArrayX.includes('3'))) {
    //  console.log('yes');
    //  }  else {
@@ -89,72 +89,14 @@ function updateCellStatus(e) {
  //playerArrayX.includes(1)) {
         //console.log(playerArrayX.includes('1'));
 
-if
-let winningCombination = 
-(1 && 2 && 3) || 
-(4 && 5 && 6) || 
-(7,8,9) || 
-(1,4,7) || 
-(2,5,8) || 
-(3,6,9) || 
-(1,5,9) || 
-(3,5,7);
-
-
- 
-
-   // if containsX = true;
-//    //  1,2,3   4,5,6   7,8,9   1,4,7  2,5,8  3,6,9  1,5,9  3,5,7
 // // function declare winnerX
 // //  or if containsO = true;
 //     //call the function/declare winnerO
 
-
-
 // //if there is a winner, disenable the clicking on the squares
-
-
-
-
-
-
-// adds X to cell
-// only if empty
-// only if no winner yet
-// //only if x's turn
-// function playX() {
-
-    
-
-
-//  //const myButton = document.getElementById('myButton').addEventListener('click', () => {
-//    //console.log("This works");
-// }
-
-
 
 
 // //once there is a winner, a viny line draws through
 // function checkForWinner() {
 //     if (containsX = true )
 // }
-
-
-
-
-
-
-
-
-
-
-// //or function x wins or function o wins
-// //butterfly flaps
-// //wreath rotates circle
-// function announceWinner(){
-
-// }
-
-
-// //reset game by emptying all cells
-// function startNewGame() 
