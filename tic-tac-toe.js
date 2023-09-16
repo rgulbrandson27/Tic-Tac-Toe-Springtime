cf2d1dc8702913e6c0d8b4070dba183601cec2b8
rgulbrandson27 <105253984+rgulbrandson27@users.noreply.github.com>
2023-09-15 22:27:52 -0500

    logic complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,7 +5,7 @@ let playerXArray = [];
 let playerOArray = [];
 
 function updateCellStatus(e) {
-  
+   if (gameActive === true) {
    const x = document.createElement("img");
    x.setAttribute("src", "images/ButterflyX.png");
    x.setAttribute("id", "butterfly");
@@ -17,53 +17,64 @@ function updateCellStatus(e) {
    if (currentPlayer === "Player X") {
       e.append(x);
       playerXArray.push(parseInt(e.id.charAt(5)));
-      
-      winCombinations.forEach((subWinArray) => {
-         let combinedArrays = new Set([...subWinArray, ...playerXArray]);
-         return (
-            combinedArrays.size === playerXArray.length ? whoseTurn.innerHTML = "X Wins!"
-            : console.log("no winner yet")
-         )});
+      $('#whoseTurn').text("O's Turn").css("color", "black");
 
-      setTimeout(() => $('#whoseTurn').text("O's Turn"), 300);
       } else {
       e.append(o);
       playerOArray.push(parseInt(e.id.charAt(5)))
-      winCombinations.forEach((subWinArray) => {
-         let combinedArrays = new Set([...subWinArray, ...playerOArray]);
-         return (
-            combinedArrays.size === playerXArray.length ? whoseTurn.innerHTML = "O Wins!": console.log("no winner yet")
-         )});
-      setTimeout(() => $('#whoseTurn').text("X's Turn"), 300);
-      }
-      
-    
-      e.removeAttribute("onclick");          /* displays not-allowed symbol over cell */
-      function changeCursor () {
-      e.style.cursor= "not-allowed";
+      $('#whoseTurn').text("X's Turn").css("color", "pink");
       }
    
-
-      setTimeout(changeCursor, 2000);
-      console.log(`Player X: [${playerXArray}]`);
+   console.log(`Player X: [${playerXArray}] Player O: [${playerOArray}]`);
+   checkForWin();
    
+   e.removeAttribute("onclick");          /* displays not-allowed symbol over cell */
+   function changeCursor () {
+   e.style.cursor= "not-allowed";
+   }
+   
+      setTimeout(changeCursor, 1000);
       currentPlayer = currentPlayer === "Player X" ? "Player O" : "Player X";
-   };
+      }
+}
 
-
-   const winCombinations = [
+function checkForWin() {
+   let winCombinations = [
       [1, 2, 3], [4, 5, 6], [7, 8, 9],     /* horizontal */
       [1, 4, 7], [2, 5, 8], [3, 6, 9],     /* vertical   */
       [1, 5, 9], [3, 5, 7]                 /* diagonal   */
    ];
+
+   if (currentPlayer === "Player X") {
+      winCombinations.forEach((subWinArray) => {
+         let combinedArrays = new Set([...subWinArray, ...playerXArray]);
+    
+         if (combinedArrays.size === playerXArray.length) {
+            whoseTurn.innerHTML = "X Wins!";
+            gameActive = false;
+            document.body.style.cursor="not-allowed";
+         }
+      
+      })
+         }  else {
+      winCombinations.forEach((subWinArray) => {
+         let combinedArrays = new Set([...subWinArray, ...playerOArray]);
+
+         if (combinedArrays.size === playerXArray.length) {
+            whoseTurn.innerHTML = "O Wins!";    
+            gameActive = false;
+            document.body.style.cursor="not-allowed";
+               }
+           })
+      
+      }
+   }
+  
 
 function resetGame(e) {
    let allCells = $('.cell');
    allCells.empty(e);
    playerXArray = [];
    playerOArray = [];  
-
-   console.log("Start New Game");
    location.reload();
 }
-console.log(`Player X: [${playerXArray}] Player O: [${playerOArray}]`);
